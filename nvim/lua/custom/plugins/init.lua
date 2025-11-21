53d78b4ab217ef14de0705944cee533c7b794bc6
sam8beard <sammybeard2002@gmail.com>
2025-11-21 01:04:35 -0500

    added nvim and oh-my-zsh configs

---
+++ nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,7 @@
+-- You can add your own plugins here or in other files in this directory!
+--
+-- See the kickstart.nvim README for more information
+return {
+  'OXY2DEV/markview.nvim',
+  lazy = false,
+}
